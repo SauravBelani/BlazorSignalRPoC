ab40ab8955d5f1c2bdd7d1c268cb2abc0a4453e3
Saurav Belani <saurav.b@simformsolutions.com>
2025-06-25 12:54:23 +0530

    Add project files.

---
+++ BlazorSignalRPoC/Hubs/EditorHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+public class EditorHub : Hub
+{
+    public async Task BroadcastText(string user, string text)
+    {
+        await Clients.Others.SendAsync("ReceiveText", user, text);
+    }
+}
